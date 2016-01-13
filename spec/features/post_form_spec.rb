652969bae7da609777068394acc899c537c1a04b
Ashi Krishnan <me@ashi.works>
2016-01-13 01:02:55 -0500

    Post feature spec

--- spec/features/post_form_spec.rb
+++ spec/features/post_form_spec.rb
@@ -1,38 +1,31 @@
 require 'rails_helper'
 require 'capybara/rspec'
 
-describe "the song form", :type => :feature do
-  # it "creates a song on submit" do
-  #   visit '/songs/new'
-  #   fill_in :song_title, with: 'Little Earthquakes'
-  #   find('input[name="commit"]').click
-  #   expect(Song.last.title).to eq 'Little Earthquakes'
-  # end
+describe "the post form", :type => :feature do
+  it "creates a post with content" do
+    visit '/posts/new'
+    fill_in :post_content, with: 'Hello'
+    find('input[name="commit"]').click    
+    expect(Post.last.content).to eq 'Hello'
+  end
 
-  # it "creates a song with an artist" do
-  #   visit '/songs/new'
-  #   fill_in :song_title, with: 'Little Earthquakes'
-  #   fill_in :song_artist_name, with: 'Tori Amos'
-  #   find('input[name="commit"]').click
-  #   expect(Song.last.artist.name).to eq 'Tori Amos'
-  #   expect(Artist.find_by(name: 'Tori Amos').songs.pluck(:title)).to include 'Little Earthquakes'
-  # end
+  it "creates an author when author fields are filled in" do
+    visit '/posts/new'
+    fill_in :author_name, with: 'Locke'
+    find('input[name="commit"]').click    
+    expect(Post.last.author.name).to eq 'Locke'
+  end
 
-  # it 'creates a song with a genre' do
-  #   visit '/songs/new'
-  #   fill_in :song_title, with: 'Little Earthquakes'
-  #   select 'Alternative', from: 'song_genre_id'
-  #   find('input[name="commit"]').click
-  #   expect(Song.last.genre.name).to eq 'Alternative'
-  #   expect(Genre.find_by(name: 'Alternative').songs.pluck(:title)).to include 'Little Earthquakes'
-  # end
-
-  # it 'creates a song with notes' do
-  #   visit '/songs/new'
-  #   fill_in :song_title, with: 'Little Earthquakes'
-  #   fill_in :song_notes_1, with: 'great piano'
-  #   fill_in :song_notes_2, with: 'inaccurate seismology'
-  #   find('input[name="commit"]').click
-  #   expect(Song.last.note_contents).to eq ['great piano', 'inaccurate seismology']
-  # end
+  it "doesn't duplicate authors" do
+    visit '/posts/new'
+    fill_in :author_name, with: 'Locke'
+    find('input[name="commit"]').click    
+    visit '/posts/new'
+    first_locke = Post.last.author
+    
+    fill_in :author_name, with: 'Locke'
+    find('input[name="commit"]').click
+    second_locke = Post.last.author
+    expect(first_lock).to eq second_locke
+  end
 end
