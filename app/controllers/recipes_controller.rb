26f82fd7d0c015054977b4b8b989fe49a122d9d6
Michael Chen <michaelchengaming@gmail.com>
2019-09-12 20:29:32 -0700

    Done.

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -9,8 +9,27 @@ class RecipesController < ApplicationController
 
   def new
     @recipe = Recipe.new
+    @recipe.ingredients.build
+    @recipe.ingredients.build
   end
 
   def create
+    recipe = Recipe.create(recipes_params)
+    redirect_to recipe
+  end
+
+  private
+
+  def recipes_params
+    params.require(:recipe).permit(
+      :title,
+      ingredients_attributes: [
+        :name,
+        :quantity
+      ]
+    )
   end
 end
+
+#Instructions
+#Build a recipe form that accepts two ingredients! It should automatically create the new Ingredient objects.
