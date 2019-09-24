4fbd62a0d92ab19df1fec4b950c96c7cfeb394a8
JenDirmeyer <dirmeyer@gmail.com>
2019-09-23 21:52:08 -0400

    Done.

--- Gemfile
+++ Gemfile
@@ -33,6 +33,7 @@ gem 'sdoc', '~> 0.4.0', group: :doc
 # gem 'capistrano-rails', group: :development
 
 group :development do
+  gem 'pry'
 end
 
 group :development, :test do
--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -9,8 +9,19 @@ class RecipesController < ApplicationController
 
   def new
     @recipe = Recipe.new
+    @recipe.ingredients.build()
+    @recipe.ingredients.build()
   end
 
   def create
+    @recipe = Recipe.create(recipe_params)
+    redirect_to @recipe
   end
+
+  private 
+
+  def recipe_params
+    params.require(:recipe).permit(:title, ingredients_attributes:[
+      :name, :quantity])
+  end  
 end
