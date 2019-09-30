0f86826a9207eb100cdbedf5222aa4e900d9b08d
github username <github email address>
2019-10-01 00:46:11 +0900

    Done.

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -9,8 +9,29 @@ class RecipesController < ApplicationController
 
   def new
     @recipe = Recipe.new
+    @recipe.ingredients.build(name: 'name')
+    @recipe.ingredients.build(quantity: 'quantity')
   end
 
   def create
+    @recipe = Recipe.create(recipe_params)
+    if @recipe.valid?
+      @recipe.save
+      redirect_to recipe_path(@recipe)
+    else
+      render :new
+    end
+
+
+  end
+
+  private
+  def recipe_params
+    params.require(:recipe).permit(:title, ingredients_attributes: [
+      :name,
+      :quantity
+    ]
+  )
+
   end
 end
--- app/models/ingredient.rb
+++ app/models/ingredient.rb
@@ -1,3 +1,8 @@
 class Ingredient < ActiveRecord::Base
   belongs_to :recipe
+
+ # def recipe_attributes=(recipe)
+   # self.recipe = Recipe.find_or_create_by(title: recipe[:title])
+   # self.recipe.update(recipe)
+ # end
 end
